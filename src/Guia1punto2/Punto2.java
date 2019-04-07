5426277436d3840dc6ccb959d95f653260787a7c
Johan Steward Galeano Torres <44073891+JStewardGT@users.noreply.github.com>
2019-04-07 15:55:10 -0500

    Update Punto2.java

--- src/Guia1punto2/Punto2.java
+++ src/Guia1punto2/Punto2.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Guia1punto2;
 
-/**
- *
- * @author Shenckeli
- */
 public class Punto2 {
 
     public static void main(String[] args) {
