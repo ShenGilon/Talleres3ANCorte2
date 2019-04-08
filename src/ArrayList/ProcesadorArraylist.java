0378274059c3615eee30c11c0a32113843e58038
Johan Steward Galeano Torres <44073891+JStewardGT@users.noreply.github.com>
2019-04-07 21:01:07 -0500

    punto 1

--- src/ArrayList/ProcesadorArraylist.java
+++ src/ArrayList/ProcesadorArraylist.java
@@ -5,11 +5,14 @@ import java.util.ArrayList;
 public class ProcesadorArraylist {
 
     public void imprimirArraylistFacturas(ArrayList<Estudiantes> arl) {
-        int i = 0, promedioGeneral=0;
+        int i = 0;
+        float promedioGeneral=0;
         System.out.println("Estudiante \t\t\t Documento\t\t\tNota 1\t\t\tNota 2\t\t\tNota 3\t\t\tDefinitiva");
         while (i < arl.size()) {
-            System.out.println(arl.get(i).Nombre + "\t\t\t" + arl.get(i).CC + "\t\t\t" + arl.get(i).nota1 + "\t\t\t" + arl.get(i).nota2 + "\t\t\t" + arl.get(i).nota3 + "\t\t\t" + arl.get(i).promedio);
+            System.out.println(arl.get(i).Nombre + "\t\t" + arl.get(i).CC + "\t\t\t" + arl.get(i).nota1 + "\t\t\t" + arl.get(i).nota2 + "\t\t\t" + arl.get(i).nota3 + "\t\t\t" + arl.get(i).promedio);
+            promedioGeneral += arl.get(i).promedio;
             i++;
         }
+        System.out.println("El promedio del curso es "+ (promedioGeneral/3));
     }
 }
